e6f1615520311e032ebc11655b362f48d3ee5352
Lucy Jimenez <lucy.jimenez.mejia@gmail.com>
2023-04-22 09:33:43 -0500

    Se aplico navigate al ItmDetail

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Card, Button } from "react-bootstrap";
 import { ItemCount } from "../ItemCount/ItemCount";
+import { useNavigate } from "react-router-dom";
 
 export const ItemDetail = ({
     id,
@@ -10,6 +11,13 @@ export const ItemDetail = ({
     imagen,
     categoria,
 }) => {
+    
+    const navigate = useNavigate()
+
+    const volverHaciaAtras = () => {
+        navigate(-1)
+    }
+    
     return (
         <div className="item">
             <Card border="info" style={{ width: "24rem" }}>
@@ -29,6 +37,7 @@ export const ItemDetail = ({
                     <br />
                     <Button variant="primary">Agregar al carrito</Button>
                 </Card.Body>
+                <Button onClick={volverHaciaAtras} className='btn btn-sucess'>Volver atras</Button>
             </Card>
         </div>
     );
