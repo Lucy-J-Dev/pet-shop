639fafa03f1c034ae55d159140345c41ed3d322c
Lucy Jimenez <lucy.jimenez.mejia@gmail.com>
2023-05-15 18:23:45 -0500

    eliminar comentarios

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -2,8 +2,6 @@ import "./ItemCount.css";
 import Button from "react-bootstrap/Button";
 
 export const ItemCount = ({ max, cantidad, modify }) => {
-    // const [counter, setCounter] = useState(1);
-
     const restar = () => {
         if (cantidad > 1) {
             modify(cantidad - 1);
